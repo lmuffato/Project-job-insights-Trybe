3139c4f7fd43abd6764e5a39617ebb8a16e635ae
Viegas <mvrdgs@gmail.com>
2021-12-02 16:07:24 -0300

    requisito 4: filter max salary

--- src/insights.py
+++ src/insights.py
@@ -67,21 +67,13 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    job_list = read(path)
+    max_salaries = set(
+        int(job["max_salary"])
+        for job in job_list
+        if job["max_salary"] != "" and job["max_salary"].isnumeric()
+    )
+    return max(max_salaries)
 
 
 def get_min_salary(path):
