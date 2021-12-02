b437403571899468d439de70ad8f0ee10eb36b8e
Marcus Hüsemann <mchusemann@gmail.com>
2021-12-02 15:37:09 -0300

    fix(insights.py): adiciona linha entre variaveis e for

--- src/insights.py
+++ src/insights.py
@@ -4,6 +4,7 @@ from src.jobs import read
 def get_unique_job_types(path):
     jobs_data = read(path)
     unique_values = set()
+
     for job in jobs_data:
         unique_values.add(job['job_type'])
     return unique_values
