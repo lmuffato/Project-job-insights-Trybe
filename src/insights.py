19c59463d66c9025bcc7e3d72618227dcb848882
Gabriel <g.pereira.a.m@gmail.com>
2021-12-02 18:59:33 -0300

    feat(Req 5) - :sparkle: Requisito 5 feito

--- src/insights.py
+++ src/insights.py
@@ -76,21 +76,16 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
+    jobs_list = read(path)
+    salaries = set()
 
-    Must call `read`
+    for job in jobs_list:
+        try:
+            salaries.add(int(job['min_salary']))
+        except ValueError:
+            pass
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    return min(salaries)
 
 
 def matches_salary_range(job, salary):
