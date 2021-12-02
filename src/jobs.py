97e54e341c97821862166a4676f4f282c4779b27
Gabriel <g.pereira.a.m@gmail.com>
2021-12-02 16:42:39 -0300

    feat(Req 1) - :sparkle: Requisito 1 feito

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,15 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    jobs = []
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
+    with open(path) as file:
+        jobs_list = csv.DictReader(file)
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+        for job in jobs_list:
+            jobs.append(job)
+
+    return jobs
