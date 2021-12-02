31da76bf0e0aa35d1d39d23397928018b6577fb3
maricegalla <maricegalla@gmail.com>
2021-12-02 20:08:25 -0300

    feat: :sparkles: Requirement 7

--- src/insights.py
+++ src/insights.py
@@ -11,21 +11,12 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
+    filtered = []
+    for job in jobs:
+        if job['job_type'] == job_type:
+            filtered.append(job)
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    pass
+    return filtered
 
 
 def get_unique_industries(path):
@@ -57,21 +48,21 @@ def filter_by_industry(jobs, industry):
 
 def get_max_salary(path):
     jobs_read = read(path)
-    my_set = []
+    my_salary = []
     for row in jobs_read:
         if row["max_salary"].isnumeric():
-            my_set.append(int(row["max_salary"]))
-            max_sal = max(my_set)
+            my_salary.append(int(row["max_salary"]))
+            max_sal = max(my_salary)
     return max_sal
 
 
 def get_min_salary(path):
     jobs_read = read(path)
-    my_set = []
+    my_salary = []
     for row in jobs_read:
         if row["min_salary"].isnumeric():
-            my_set.append(int(row["min_salary"]))
-            min_sal = min(my_set)
+            my_salary.append(int(row["min_salary"]))
+            min_sal = min(my_salary)
     return min_sal
 
 
