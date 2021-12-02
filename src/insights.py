d54c6e7fe856de087512f1ebff3c5ab02d2ce3a8
Felipe Flores <felipefloresvendas@gmail.com>
2021-12-02 16:15:01 -0300

    :sparkles: feat requirement 04

--- src/insights.py
+++ src/insights.py
@@ -50,21 +50,13 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
+    list = read(path)
+    salary_list = []
+    for job in list:
+        if (job["max_salary"].isnumeric()):
+            salary_list.append(int(job["max_salary"]))
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    return max(salary_list)
 
 
 def get_min_salary(path):
