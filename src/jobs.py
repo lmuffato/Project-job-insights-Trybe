fd27e6130bab6a8389a2267bad202682067a7bbe
Hernanne Domiciano <78621976+hernannegp@users.noreply.github.com>
2021-12-02 15:25:40 -0300

    feat: req1

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,12 @@
+import csv
 from functools import lru_cache
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    try:
+        with open(path, 'r') as file:
+            reader = csv.DictReader(file)
+            return [row for row in reader]
+    except OSError:
+        print('Deu erro ao abrir o arquivo')
