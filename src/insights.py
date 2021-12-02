5261d843764b8768a55ccfd85fde35979a8377c5
Rafael Gerônimo <rafael@geronimo.dev>
2021-12-02 18:04:00 -0300

    feat: :sparkles: req3: `get_unique_industries` function

--- src/insights.py
+++ src/insights.py
@@ -57,8 +57,12 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-
-    return []
+    jobs = read(path)
+    industries = set()
+    for job in jobs:
+        if job["industry"] != "":
+            industries.add(job["industry"])
+    return industries
 
 
 def filter_by_industry(jobs, industry):
