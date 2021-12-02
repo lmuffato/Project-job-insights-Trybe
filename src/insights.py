9e37f92b7efb2679139935ad9f8c4baf5c0f697f
Viegas <mvrdgs@gmail.com>
2021-12-02 15:46:21 -0300

    requisito 2: filtro de job_types

--- src/insights.py
+++ src/insights.py
@@ -1,19 +1,11 @@
+from .jobs import read
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
+    job_list = read(path)
+    unique_jobs = set(job["job_type"] for job in job_list)
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    return unique_jobs
 
 
 def filter_by_job_type(jobs, job_type):
--- src/jobs.py
+++ src/jobs.py
@@ -7,5 +7,4 @@ def read(path):
     with open(path) as file:
         content = csv.DictReader(file, delimiter=",", quotechar='"')
         job_list = [row for row in content]
-
     return job_list
