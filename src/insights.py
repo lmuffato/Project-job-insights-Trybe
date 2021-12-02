0499d898e53cd73d45c2c2cc94669f05bf744e5a
Gui-lira <gui.lira.martins@gmail.com>
2021-12-02 15:08:34 -0300

    9

--- src/insights.py
+++ src/insights.py
@@ -72,18 +72,12 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    filtered = []
+    for job in jobs:
+        try:
+            condition = matches_salary_range(job, salary)
+            if condition:
+                filtered.append(job)
+        except ValueError:
+            print('esse não pode')
+    return filtered
