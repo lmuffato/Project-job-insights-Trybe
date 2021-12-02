284ce95d272807fa4e355573814b71b32cb97e1b
Jodiel Briesemeister <jodielll29@gmail.com>
2021-12-02 15:37:30 -0300

    feat: req03

--- src/insights.py
+++ src/insights.py
@@ -29,21 +29,14 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    industries = set()
+    industry_list = read(path)
 
-    Must call `read`
+    for row in industry_list:
+        if row["industry"] != "":
+            industries.add(row["industry"])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return industries
 
 
 def filter_by_industry(jobs, industry):
