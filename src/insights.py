34019124843fda9f1e5d4d5bce434e3fe130e945
Hernanne Domiciano <78621976+hernannegp@users.noreply.github.com>
2021-12-02 19:07:16 -0300

    feat: req08 função que filtra por salario desejado

--- src/insights.py
+++ src/insights.py
@@ -56,29 +56,18 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    if type(salary) is not int:
+        raise ValueError('type a valid salary')
+    if not ('max_salary' in job and 'min_salary' in job):
+        raise ValueError('not exists this key in job dict')
+    if (
+        type(job['max_salary']) is not int or
+        type(job['min_salary']) is not int
+    ):
+        raise ValueError('salary must be a integers')
+    if job['max_salary'] < job['min_salary']:
+        raise ValueError('invalid salary')
+    return job['min_salary'] <= salary <= job['max_salary']
 
 
 def filter_by_salary_range(jobs, salary):
