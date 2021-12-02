24d0b606d710fc39e16acd12bcb7f508c55f661f
luanlsr <luanfswd@gmail.com>
2021-12-02 20:53:06 -0300

    feat: :sparkles: req 2 finalizado

--- src/insights.py
+++ src/insights.py
@@ -1,19 +1,10 @@
+from jobs import read
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    list = read(path)
+    dic_types = set(job["job_type"] for job in list)
+    return dic_types
 
 
 def filter_by_job_type(jobs, job_type):
