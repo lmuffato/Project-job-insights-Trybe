c7d82184e3ffc85e16435f68a164c588be8a49b6
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-12-02 18:49:14 -0300

    feat: start requirement 6

--- src/insights.py
+++ src/insights.py
@@ -38,7 +38,9 @@ def filter_by_job_type(jobs, job_type):
     list
         List of jobs with provided job_type
     """
-    return []
+    return [job
+            for job in jobs
+            if job["job_type"] == job_type]
 
 
 def get_unique_industries(path):
