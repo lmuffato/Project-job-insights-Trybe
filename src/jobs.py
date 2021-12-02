7b4cfbb9c9a7bf4b5d0ee02323f99e2cbfed97ef
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-12-02 11:16:18 -0300

    feat: requisito 1 feito

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,12 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    try:
+        with open("jobs.csv", "r") as file:
+            content = csv.DictReader(file)
+            return[line for line in content]
+    except OSError:
+        print('Problema na localização do arquivo')
