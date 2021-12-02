181189ee0bf446de4440ed6e569e5fb0a1b7d455
maricegalla <maricegalla@gmail.com>
2021-12-02 17:09:53 -0300

    feat: :sparkles: Requirement 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,17 +1,14 @@
 from functools import lru_cache
 import csv
 
-path = "src/jobs.csv"
-
 
 @lru_cache
 def read(path):
-    with open(path) as file:
-        for index in csv.DictReader(file):
-            read_jobs = dict(index)
-    return [print(read_jobs)]
+    my_dict = []
+    with open(path, 'r') as data:
+        for line in csv.DictReader(data):
+            my_dict.append(line)
 
-
-read(path)
+    return my_dict
 
 # https://stackoverflow.com/questions/2387697/best-way-to-convert-csv-data-to-dict
