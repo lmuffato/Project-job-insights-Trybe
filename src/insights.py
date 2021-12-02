377ef9d6a9249f4aa8e5a336846f52192cd874d7
MarcioFujita <hanker78@gmail.com>
2021-12-02 16:57:55 -0300

    feat: Requisito 5

--- src/insights.py
+++ src/insights.py
@@ -124,7 +124,14 @@ def get_min_salary(path):
     int
         The minimum salary paid out of all job opportunities
     """
-    pass
+    jobs = read(path)
+    all_salary = set()
+    for job in jobs:
+        if job["min_salary"].isnumeric():
+            all_salary.add(int(job["min_salary"]))
+
+    min_salary = sorted(all_salary)
+    return min_salary[0]
 
 
 def matches_salary_range(job, salary):
