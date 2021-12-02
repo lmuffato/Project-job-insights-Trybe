6e4257058d123af1223e52014766eb02fe1f6864
MarcioFujita <hanker78@gmail.com>
2021-12-02 16:26:21 -0300

    feat: Requisito 3

--- src/insights.py
+++ src/insights.py
@@ -57,7 +57,13 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    jobs = read(path)
+    all_industries = set()
+    for job in jobs:
+        if job["industry"]:
+            all_industries.add(job["industry"])
+
+    return jobs
 
 
 def filter_by_industry(jobs, industry):
