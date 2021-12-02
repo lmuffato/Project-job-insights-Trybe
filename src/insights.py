fa798e038f2259d9fc1a94ad5532f435c4570921
Marcus Hüsemann <mchusemann@gmail.com>
2021-12-02 15:26:02 -0300

    feat(insights.py): adiciona funçao get_unique_job_types

--- src/insights.py
+++ src/insights.py
@@ -1,19 +1,12 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    jobs_data = read(path)
+    unique_values = set()
+    for job in jobs_data:
+        unique_values.add(job['job_type'])
+    return unique_values
 
 
 def filter_by_job_type(jobs, job_type):
