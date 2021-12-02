2b999c12a9479b29c378cdb02460d788dd7961d4
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-12-02 15:48:14 -0300

    Iniciando projeto

--- src/jobs.py
+++ src/jobs.py
@@ -4,12 +4,10 @@ from functools import lru_cache
 @lru_cache
 def read(path):
     """Reads a file from a given path and returns its contents
-
     Parameters
     ----------
     path : str
         Full path to file
-
     Returns
     -------
     list
