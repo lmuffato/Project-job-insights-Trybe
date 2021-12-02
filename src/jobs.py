413e4cfeb314a69126d76b536c4fde3f7170bdd5
Ana Ventura <anaventura1811@gmail.com>
2021-12-02 14:42:56 -0300

    feat: implementa função reader, req. 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,10 +1,10 @@
 from functools import lru_cache
-
+import csv
 
 @lru_cache
-def read(path):
+def read(path):      
     """Reads a file from a given path and returns its contents
-
+    
     Parameters
     ----------
     path : str
@@ -16,4 +16,13 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+    with open(path, mode="r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        data = [row for row in reader]
+    return data
+
+    # Sources:
+    # https://docs.python.org/3/library/csv.html#module-csv
+    # https://www.delftstack.com/howto/python/python-csv-to-dictionary/
+    # https://www.geeksforgeeks.org/load-csv-data-into-list-and-dictionary-using-python/
+
