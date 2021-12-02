12542ebcd12b10840026ceecf0e5e5a75974b991
rogerio-lambert <rogerio.cl@gmail.com>
2021-12-02 19:14:14 -0300

    feat(req1): create function jobs

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,10 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    with open(path) as file:
+        info_jobs = csv.DictReader(file, delimiter=",", quotechar='"')
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    return info_jobs
