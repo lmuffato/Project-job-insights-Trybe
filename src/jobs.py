5bb17f88bf618df51b2f6e2795624dc9c1ab7faa
Lucas André <lucas.cancado89@gmail.com>
2021-12-02 15:00:55 -0300

    Req1: read csv file

--- src/jobs.py
+++ src/jobs.py
@@ -5,5 +5,5 @@ import csv
 @lru_cache
 def read(path):
     with open(path) as file:
-        dictionary_list = csv.reader(file, delimiter=",")
+        dictionary_list = csv.DictReader(file)
     return dictionary_list
