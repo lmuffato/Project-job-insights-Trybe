1f459787c54015cb06b93e48ddfe434f8eb39dd4
giovanni-maldonado <giovannitadeu08@gmail.com>
2021-12-02 18:12:09 -0300

    feat: função para filtrar um emprego por tipo

--- src/insights.py
+++ src/insights.py
@@ -10,21 +10,11 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    filter_list = []
+    for job in jobs:
+        if job['job_type'] == job_type:
+            filter_list.append(job)
+    return filter_list
 
 
 def get_unique_industries(path):
