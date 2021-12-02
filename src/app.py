28d98b995b3df251c6f32d558ef451dadc847a4c
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-12-02 15:30:37 -0300

    Inicia o projeto

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from . import routes_and_views
 
 
-def create_app() -> Flask:
+def create_app() -> Flask:  # create_app
     app = Flask(__name__)
     routes_and_views.init_app(app)
 
