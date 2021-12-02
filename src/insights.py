2bd3c428a2d0fd98e5b893d500304d724353639e
Marcelofcdantas <65692996+Marcelofcd@users.noreply.github.com>
2021-12-02 17:51:36 -0300

    requisito 8 aprovado

--- src/insights.py
+++ src/insights.py
@@ -66,31 +66,23 @@ def get_min_salary(path):
             result = int(info[min_salary])
     return result
 
+# lançamento de erro visto no repositório do Diego-F-Affonso
+# https://github.com/tryber/sd-09-project-job-insights/pull/96
+
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    pass
+    all_salaries = "job['min_salary'] or job['max_salary']"
+    if ('min_salary' not in job or 'max_salary' not in job):
+        raise ValueError('Error')
+    if not all_salaries.rstrip():
+        raise ValueError('Error')
+    if not (type(salary) == int):
+        raise ValueError('Error')
+    if not (type(job["min_salary"]) == int or type(job["max_salary"]) == int):
+        raise ValueError('Error')
+    if int(job['min_salary']) > int(job['max_salary']):
+        raise ValueError('Error')
+    return job["min_salary"] <= salary <= job["max_salary"]
 
 
 def filter_by_salary_range(jobs, salary):
