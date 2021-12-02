9529d04a49c092258c84a7b2e215193224d32f05
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-12-02 17:28:21 -0300

    Feat: (req-01) Implementa funçao read

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,17 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    jobs_list = []
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
+    try:
+        with open(path) as file:
+            data = csv.DictReader(file)
+            for result in data:
+                jobs_list.append(result)
+    except OSError:
+        print("arquivo inexistente")
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    return jobs_list
