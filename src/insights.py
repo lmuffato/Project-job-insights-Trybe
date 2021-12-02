1dec5183b69a19e96f24e1325be5b67914b3bb60
giovanni-maldonado <giovannitadeu08@gmail.com>
2021-12-02 19:16:45 -0300

    feat: função para filtrar a faixa salarial

--- src/insights.py
+++ src/insights.py
@@ -70,18 +70,11 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    filter_list = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                filter_list.append(job)
+        except ValueError:
+            print("Invalid salary range")
+    return filter_list
