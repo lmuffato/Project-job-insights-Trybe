c599f5653556f6e7ee4b573b4af9a369abc09dc3
Carolina Vasconcelos <carolinavpinheiro@gmail.com>
2021-12-03 16:52:01 -0300

    Feat: desafio 9

--- src/insights.py
+++ src/insights.py
@@ -64,18 +64,12 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
+    list_jobs_by_salary = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                list_jobs_by_salary.append(job)
+        except ValueError:
+            pass
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    return list_jobs_by_salary
