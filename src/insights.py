a9808978ace54eb536332d6ed794f8868a490bf5
Marcus Hüsemann <mchusemann@gmail.com>
2021-12-03 11:58:05 -0300

    refactor(insights.py): adiciona algumas funçoes genericas para melhorar legibilidade

--- src/insights.py
+++ src/insights.py
@@ -2,12 +2,34 @@ from src.jobs import read
 import sys
 
 
+def is_not_empty(*values):
+    is_valid_values = True
+    for value in values:
+        if(value == ''):
+            is_valid_values = False
+            break
+    return is_valid_values
+
+
+def is_int(*values):
+    is_valid_values = True
+    for value in values:
+        if(type(value) != int):
+            is_valid_values = False
+            break
+    return is_valid_values
+
+
+def is_not_alpha(value):
+    return not value.isalpha()
+
+
 def get_unique_job_types(path):
     jobs_data = read(path)
     unique_job_types = set()
 
     for job in jobs_data:
-        if job["job_type"] != "":
+        if is_not_empty(job["job_type"]):
             unique_job_types.add(job["job_type"])
 
     return unique_job_types
@@ -26,7 +48,7 @@ def get_unique_industries(path):
     jobs_data = read(path)
     unique_industries = set()
     for job in jobs_data:
-        if job["industry"] != "":
+        if is_not_empty(job["industry"]):
             unique_industries.add(job["industry"])
 
     return unique_industries
@@ -46,7 +68,8 @@ def get_max_salary(path):
     max_salary = 0
 
     for job in jobs_data:
-        if(job['max_salary'] != '' and not job['max_salary'].isalpha()):
+        if(is_not_empty(job['max_salary']) and
+           is_not_alpha(job['max_salary'])):
             salary = int(job['max_salary'])
             if(salary > max_salary):
                 max_salary = salary
@@ -60,7 +83,8 @@ def get_min_salary(path):
     min_salary = sys.maxsize
 
     for job in jobs_data:
-        if(job['min_salary'] != '' and not job['min_salary'].isalpha()):
+        if(is_not_empty(job['min_salary']) and
+           is_not_alpha(job['min_salary'])):
             salary = int(job['min_salary'])
             if(salary < min_salary):
                 min_salary = salary
@@ -74,14 +98,11 @@ def matches_salary_range(job, salary):
 
     max_salary = job['max_salary']
     min_salary = job['min_salary']
-    max_salary_is_int = type(max_salary) is not int
-    min_salary_is_int = type(min_salary) is not int
-    salary_is_int = type(salary) is not int
 
-    if(max_salary_is_int or min_salary_is_int or salary_is_int):
-        raise ValueError('not int')
+    if(not is_int(max_salary, min_salary, salary)):
+        raise ValueError('Some value is not int')
     elif(min_salary > max_salary):
-        raise ValueError('not valid')
+        raise ValueError('min_salary greater than max_salary')
     else:
         return min_salary <= salary <= max_salary
 
@@ -92,13 +113,13 @@ def filter_by_salary_range(jobs, salary):
     for job in jobs:
         if('min_salary' not in job or 'max_salary' not in job):
             continue
+
         min_salary = job['min_salary']
         max_salary = job['max_salary']
-        is_valid_salary = salary != '' and type(salary) == int
-        is_valid_min_salary = min_salary != '' and type(min_salary) == int
-        is_valid_max_salary = max_salary != '' and type(max_salary) == int
 
-        if(is_valid_max_salary and is_valid_min_salary and is_valid_salary):
+        if(is_not_empty(min_salary, max_salary, salary) and
+           is_int(min_salary, max_salary, salary)):
             if(min_salary <= salary <= max_salary):
                 list_of_jobs.append(job)
+
     return list_of_jobs
