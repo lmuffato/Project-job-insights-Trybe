1f8514d6a8152a3a59e8428472f37139da16b55f
Marcelo Maurício <mmauricio.design@gmail.com>
2021-12-03 15:56:48 -0300

    Feat 1: created a function to read a csv file and return a list with dict

--- src/jobs.py
+++ src/jobs.py
@@ -18,8 +18,11 @@ def read(path):
         List of rows as dicts
     """
     with open(path) as file:
-        jobs_list = csv.reader(file, delimiter=",", quotechar='"')
+        jobs_list = csv.DictReader(file, delimiter=",", quotechar='"')
 
-        # jobs_by_
+        csv_to_list = list(jobs_list)
 
-    return []
+    return csv_to_list
+
+# https://www.tutorialspoint.com/python/list_list.htm
+# https://docs.python.org/3/library/csv.html#csv.DictReader
