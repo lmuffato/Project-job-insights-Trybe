59dbb25f7e1d84ff678831b3e771eed73c17d23c
iago <iago.pferreiravr@gmail.com>
2021-12-03 09:34:42 -0300

    feat(test_sorting): Req10
    Implementa o teste da função sort_by

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,43 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+
+test_jobs_list = [
+    {
+        "name": "Veterinária",
+        "min_salary": 2800,
+        "max_salary": 5900,
+        "date_posted": 2021-10-22,
+    },
+    {
+        "name": "Advogado",
+        "min_salary": 4000,
+        "max_salary": 7900,
+        "date_posted": 2020-12-4,
+    },
+    {
+        "name": "Psicologa",
+        "min_salary": 2200,
+        "max_salary": 6000,
+        "date_posted": 2021-9-16,
+    }
+]
+
+result_expect_when_sorted_by_min_salary = [
+    test_jobs_list[2],
+    test_jobs_list[0],
+    test_jobs_list[1]
+]
+
+result_expect_when_sorted_by_max_salary = [
+    test_jobs_list[1],
+    test_jobs_list[2],
+    test_jobs_list[0]
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(test_jobs_list, "min_salary")
+    assert(test_jobs_list == result_expect_when_sorted_by_min_salary)
+
+    sort_by(test_jobs_list, "max_salary")
+    assert(test_jobs_list == result_expect_when_sorted_by_max_salary)
