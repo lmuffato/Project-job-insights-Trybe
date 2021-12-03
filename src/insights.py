8a68997370d8f192937bf7ccc171cbb8858f4d46
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-12-03 15:35:02 -0300

    Feat: (req-6) Implementa a funçao filter_by_job_type

--- src/insights.py
+++ src/insights.py
@@ -15,21 +15,16 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
+    job_list = []
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
+    try:
+        for job in jobs:
+            if job['job_type'] == job_type:
+                job_list.append(job)
+    except ValueError:
+        print("Variable 'jobs_list' is not defined")
 
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    return job_list
 
 
 def get_unique_industries(path):
