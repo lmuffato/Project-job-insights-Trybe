8525663c8f88ce597cd4ee6174a56f6fd2ef82b8
Renan Oliveira <oliveira.renan.rg@gmail.com>
2021-12-03 16:51:15 -0300

    start: requirement 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,73 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {"max_salary": 1000, "min_salary": 100, "date_posted": "2021-02-01"},
+        {"max_salary": 2000, "min_salary": 200, "date_posted": "2021-03-01"},
+        {"max_salary": 3000, "min_salary": 300, "date_posted": "2021-01-01"},
+    ]
+    results_expected = [
+        {
+            "criteria": "max_salary",
+            "result": [
+                {
+                    "max_salary": 3000,
+                    "min_salary": 300,
+                    "date_posted": "2021-01-01",
+                },
+                {
+                    "max_salary": 2000,
+                    "min_salary": 200,
+                    "date_posted": "2021-03-01",
+                },
+                {
+                    "max_salary": 1000,
+                    "min_salary": 100,
+                    "date_posted": "2021-02-01",
+                },
+            ],
+        },
+        {
+            "criteria": "min_salary",
+            "result": [
+                {
+                    "max_salary": 1000,
+                    "min_salary": 100,
+                    "date_posted": "2021-02-01",
+                },
+                {
+                    "max_salary": 2000,
+                    "min_salary": 200,
+                    "date_posted": "2021-03-01",
+                },
+                {
+                    "max_salary": 3000,
+                    "min_salary": 300,
+                    "date_posted": "2021-01-01",
+                },
+            ],
+        },
+        {
+            "criteria": "date_posted",
+            "result": [
+                {
+                    "max_salary": 2000,
+                    "min_salary": 200,
+                    "date_posted": "2021-03-01",
+                },
+                {
+                    "max_salary": 1000,
+                    "min_salary": 100,
+                    "date_posted": "2021-02-01",
+                },
+                {
+                    "max_salary": 3000,
+                    "min_salary": 300,
+                    "date_posted": "2021-01-01",
+                },
+            ],
+        },
+    ]
+    for test in results_expected:
+        assert sort_by(jobs, test["criteria"]) == test["result"]
