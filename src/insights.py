0c990e145864bb0ac8c9d873a59ea60201d1af88
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-12-03 00:56:41 -0300

    Req 2: get job_types

--- src/insights.py
+++ src/insights.py
@@ -1,3 +1,6 @@
+from src.jobs import read
+
+
 def get_unique_job_types(path):
     """Checks all different job types and returns a list of them
 
@@ -13,7 +16,11 @@ def get_unique_job_types(path):
     list
         List of unique job types
     """
-    return []
+    list_jobs = set()
+    reader_datas = read(path)
+    for row in reader_datas:
+        list_jobs.add(row["job_type"])
+    return list_jobs
 
 
 def filter_by_job_type(jobs, job_type):
