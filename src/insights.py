0dc99dff2f3c177daa6b5e4c5816c4eaf4c15fa2
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-12-03 19:39:54 -0300

    create fct get_max_salary

--- src/insights.py
+++ src/insights.py
@@ -55,21 +55,12 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    jobs_list = read(path)
+    salary_list = []
+    for job in jobs_list:
+        if job["max_salary"].isdigit():
+            salary_list.append(int(job["max_salary"]))
+    return max(salary_list)
 
 
 def get_min_salary(path):
