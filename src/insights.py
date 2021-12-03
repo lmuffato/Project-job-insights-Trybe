1f6472d8a5afcca5d45ba92401a8eca6d8228576
Ana Ventura <anaventura1811@gmail.com>
2021-12-02 21:01:57 -0300

    remove comentários desnecessários

--- src/insights.py
+++ src/insights.py
@@ -1,5 +1,4 @@
 from src import jobs
-# import dict
 
 
 def get_unique_values(key, vector):
@@ -22,95 +21,28 @@ def filter_by_param(key, category_name, vector):
 
 
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
-    Must call `read`
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-    Returns
-    -------
-    list
-        List of unique job types
-    """
     list_of_dicts = jobs.read(path)
     jobs_types_list = get_unique_values('job_type', list_of_dicts)
     return jobs_types_list
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
     result = filter_by_param('job_type', job_type, jobs)
     return result
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
     list_of_dicts = jobs.read(path)
     unique_industries = get_unique_values('industry', list_of_dicts)
     return unique_industries
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
     result = filter_by_param('industry', industry, jobs)
     return result
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
     salary = 0
     list_of_dicts = jobs.read(path)
     list_of_salaries = get_unique_values('max_salary', list_of_dicts)
@@ -121,20 +53,6 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
     min_salary = 0
     list_of_dicts = jobs.read(path)
     list_of_salaries = get_unique_values('min_salary', list_of_dicts)
@@ -146,27 +64,6 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
     if (type(salary) is not int
         or "min_salary" not in job
         or "max_salary" not in job
@@ -178,20 +75,6 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
     list_of_jobs = []
     for job in jobs:
         try:
