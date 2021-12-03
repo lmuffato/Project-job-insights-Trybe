125ab63ff20df0fd2ea5a19f11415cad23853b8b
Rodrigo Rech <rodrigorech92@gmail.com>
2021-12-03 16:13:52 -0300

    Feat: test sort_by with right values

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,40 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+jobs = [
+    {"min_salary": 1, "max_salary": 25, "date_posted": "2021-12-03"},
+    {"min_salary": 500, "max_salary": 15000, "date_posted": "2021-05-05"},
+    {"min_salary": 2000, "max_salary": 13000, "date_posted": "2010-12-25"},
+    {"min_salary": 9000, "max_salary": 20000, "date_posted": "2019-07-25"},
+]
+
+mock_min_salary = [
+    {"min_salary": 1, "max_salary": 25, "date_posted": "2021-12-03"},
+    {"min_salary": 500, "max_salary": 15000, "date_posted": "2021-05-05"},
+    {"min_salary": 2000, "max_salary": 13000, "date_posted": "2010-12-25"},
+    {"min_salary": 9000, "max_salary": 20000, "date_posted": "2019-07-25"},
+]
+
+mock_max_salary = [
+      {"min_salary": 9000, "max_salary": 20000, "date_posted": "2019-07-25"},
+      {"min_salary": 500, "max_salary": 15000, "date_posted": "2021-05-05"},
+      {"min_salary": 2000, "max_salary": 13000, "date_posted": "2010-12-25"},
+      {"min_salary": 1, "max_salary": 25, "date_posted": "2021-12-03"},
+  ]
+
+mock_date_posted = [
+    {"min_salary": 1, "max_salary": 25, "date_posted": "2021-12-03"},
+    {"min_salary": 500, "max_salary": 15000, "date_posted": "2021-05-05"},
+    {"min_salary": 9000, "max_salary": 20000, "date_posted": "2019-07-25"},
+    {"min_salary": 2000, "max_salary": 13000, "date_posted": "2010-12-25"},
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs, 'min_salary')
+    assert jobs == mock_min_salary
+
+    sort_by(jobs, 'max_salary')
+    assert jobs == mock_max_salary
+
+    sort_by(jobs, 'date_posted')
+    assert jobs == mock_date_posted
