58541a36a172a65c6f13598cb33174fffc59a71d
biazidioti <bzferreira.bio@gmail.com>
2021-12-03 16:45:42 -0300

    Read a csv file

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,12 @@
+import csv
 from functools import lru_cache
 
 # ler arquivo
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path, 'r') as data:
+        jobs = []
+        jobs_list = csv.DictReader(data)
+        for elemento in jobs_list:
+            jobs.append(elemento)
+    return jobs
