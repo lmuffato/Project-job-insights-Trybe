ef1c163d5c4307c99ffd5774cfa50d0ea3119727
Gabriel <g.pereira.a.m@gmail.com>
2021-12-03 18:32:34 -0300

    feat(Req 9) - :sparkle: Requisito 9 feito

--- src/insights.py
+++ src/insights.py
@@ -96,18 +96,11 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    list_jobs_salary = []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                list_jobs_salary.append(job)
+        except ValueError:
+            pass
+    return list_jobs_salary
