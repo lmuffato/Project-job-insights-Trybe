d761dc10439ca97710e8425ade902ccb9cb39d5b
Mateus <mtauws@gmail.com>
2021-12-03 10:55:49 -0300

    feat: create function read on jobs.py

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,22 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    answer = []
+    helper = []
+    counter = 1
+    with open(path) as file:
+        reader = csv.reader(file)
+        for line in reader:
+            helper.append(line)
+    while counter < len(helper):
+        dic = {}
+        holder = 0
+        for item in helper[counter]:
+            dic[helper[0][holder]] = item
+            holder += 1
+        answer.append(dic)
+        counter += 1
+    return answer
