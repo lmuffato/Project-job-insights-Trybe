555f4e2c5a8e97f058633203762b65460a0c136c
mallmann02 <leomallmann020302@gmail.com>
2021-12-03 10:58:37 -0300

    fix: bug in application
    Cannot pass arguments to the function specifying the key

--- src/routes_and_views.py
+++ src/routes_and_views.py
@@ -62,7 +62,7 @@ def list_jobs():
 
 @bp.route("/job/<index>")
 def job(index):
-    jobs = read(path="src/jobs.csv")
+    jobs = read("src/jobs.csv")
     job = get_job(jobs, index)
 
     return render_template("job.jinja2", job=job)
