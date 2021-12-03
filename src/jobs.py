711cd8502b6dd08060bddf278832c84c4ffbab38
biazidioti <bzferreira.bio@gmail.com>
2021-12-03 17:02:50 -0300

    Retorna industrias

--- src/jobs.py
+++ src/jobs.py
@@ -1,12 +1,9 @@
 import csv
 from functools import lru_cache
 
+
 # ler arquivo
 @lru_cache
 def read(path):
     with open(path, 'r') as data:
-        jobs = []
-        jobs_list = csv.DictReader(data)
-        for elemento in jobs_list:
-            jobs.append(elemento)
-    return jobs
+        return list(csv.DictReader(data))
