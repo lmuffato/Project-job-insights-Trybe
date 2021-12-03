8e143465f0029fc1e6f876ae9f6cf861709b20ad
nascjoao <oi@joaonasc.dev>
2021-12-03 15:54:42 -0300

    feat: get unique value by industry

--- src/insights.py
+++ src/insights.py
@@ -24,21 +24,7 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return get_unique_values(path, 'industry')
 
 
 def filter_by_industry(jobs, industry):
