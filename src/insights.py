015fd2c065acf74963638754aa8a1de146a08caa
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-12-03 17:45:58 -0300

    create unique_job_types and unique_industries fct

--- src/insights.py
+++ src/insights.py
@@ -1,19 +1,13 @@
+from src.jobs import read
+
+# from jobs import read
+
+
 def get_unique_job_types(path):
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    return []
+    jobs_list = read(path)
+    unique_jobs_types = set(job["job_type"] for job in jobs_list)
+    print(unique_jobs_types)
+    return unique_jobs_types
 
 
 def filter_by_job_type(jobs, job_type):
@@ -35,21 +29,11 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    industries_list = read(path)
+    unique_industries = set(
+        industry["industry"] for industry in industries_list if industry["industry"]
+    )
+    return unique_industries
 
 
 def filter_by_industry(jobs, industry):
