cbd25b283671cb8017b6de125e876b9606aa2f18
Luciano Amâncio <madruga665@hotmail.com>
2021-12-03 17:34:33 -0300

    requisto 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,8 +1,9 @@
 from src.sorting import sort_by
+import pytest
 
 jobs_list = [
-    {"min_salary": 0, "max_salary": 10, "date_posted": "01/12/2021"},
     {"min_salary": 10, "max_salary": 30, "date_posted": "02/12/2021"},
+    {"min_salary": 0, "max_salary": 10, "date_posted": "01/12/2021"},
     {"min_salary": 20, "max_salary": 50, "date_posted": "03/12/2021"},
 ]
 
@@ -19,9 +20,9 @@ sort_by_max_salary = [
 ]
 
 sort_by_date_posted = [
-    {"min_salary": 0, "max_salary": 10, "date_posted": "01/12/2021"},
-    {"min_salary": 10, "max_salary": 30, "date_posted": "02/12/2021"},
     {"min_salary": 20, "max_salary": 50, "date_posted": "03/12/2021"},
+    {"min_salary": 10, "max_salary": 30, "date_posted": "02/12/2021"},
+    {"min_salary": 0, "max_salary": 10, "date_posted": "01/12/2021"},
 ]
 
 
@@ -34,3 +35,6 @@ def test_sort_by_criteria():
 
     sort_by(jobs_list, "date_posted")
     assert sort_by_date_posted
+
+    with pytest.raises(ValueError, match="invalid sorting criteria: Xablau"):
+        sort_by(jobs_list, "Xablau")
