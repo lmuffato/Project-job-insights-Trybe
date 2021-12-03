5df34c33b9e296731b9ab8fd7deb652572ef6a38
Ana Ventura <anaventura1811@gmail.com>
2021-12-02 22:17:59 -0300

    feat: implementa req. bônus

--- src/routes_and_views.py
+++ src/routes_and_views.py
@@ -11,7 +11,7 @@ from .insights import (
     get_min_salary,
     get_max_salary,
 )
-from .more_insights import slice_jobs, get_int_from_args, build_jobs_urls
+from .more_insights import slice_jobs, get_int_from_args, build_jobs_urls, get_job
 
 bp = Blueprint("client", __name__, template_folder="templates")
 
@@ -57,6 +57,13 @@ def list_jobs():
     }
 
     return render_template("list_jobs.jinja2", ctx=ctx)
+
+@bp.route("/job/<index>")
+def job(index):
+    list_of_jobs = read("src/jobs.csv")
+    single_job = get_job(list_of_jobs, index)
+
+    return render_template("job.jinja2", job=single_job)
 
 
 def init_app(app: Flask):
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,14 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
 
+mock_jobs = [
+    {"max_salary": "3500", "min_salary": "6000", "date_posted": "2021-10-20"},
+    {"max_salary": "1000", "min_salary": "1800", "date_posted": "2021-11-20"},
+    {"max_salary": "8000", "min_salary": "15000", "date_posted": "2021-12-01"},
+    {"max_salary": "1500", "min_salary": "8000", "date_posted": "2021-11-18"},
+    {"max_salary": "1500", "min_salary": "3500", "date_posted": "2021-10-31"},
+    {"max_salary": "700", "min_salary": "1200", "date_posted": "2021-11-18"},
+]
 
 def test_sort_by_criteria():
     pass
