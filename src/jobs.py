6d1e11c44854c14675f86c1426b5ce6606fafb59
nascjoao <oi@joaonasc.dev>
2021-12-03 15:51:42 -0300

    feat: function to get unique values

--- src/jobs.py
+++ src/jobs.py
@@ -10,3 +10,12 @@ def read(path):
         for row in file_reader:
             list_of_dicts.append(row)
         return list_of_dicts
+
+
+def get_unique_values(path, column):
+    dict_list = read(path)
+    set_to_return = set()
+    for value in dict_list:
+        if (value != ''):
+            set_to_return.add(value[column])
+    return set_to_return
