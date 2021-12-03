17fa781101c49d9e4dc8a61190ceecbee57bb10b
Rafael Gerônimo <rafael@geronimo.dev>
2021-12-03 16:03:38 -0300

    feat: :sparkles: req10.1: `sort_by` test

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,26 @@
-# from src.sorting import sort_by
+import pytest
+from src.sorting import sort_by
 
+
+jobs = [
+    {"max_salary": 1000, "min_salary": 100,"date_posted": "2020-05-02"},
+    {"max_salary": 212901, "min_salary": 125410,"date_posted": "2020-04-28"},
+    {"max_salary": 103279, "min_salary": 94715,"date_posted": "2020-05-05"},
+    {"max_salary": 35000, "min_salary": 20000,"date_posted": "2020-05-07"},
+    {"max_salary": 143860, "min_salary": 54991,"date_posted": "2020-05-08"},
+    {"max_salary": 55893, "min_salary": 46298,"date_posted": "2020-05-01"},
+    {"max_salary": 148734, "min_salary": 122296,"date_posted": "2020-04-28"},
+]
 
 def test_sort_by_criteria():
-    pass
+    
+    sort_by(jobs, "min_salary")
+    assert jobs == [
+        {"max_salary": 1000, "min_salary": 100,"date_posted": "2020-05-02"},
+        {"max_salary": 35000, "min_salary": 20000,"date_posted": "2020-05-07"},
+        {"max_salary": 55893, "min_salary": 46298,"date_posted": "2020-05-01"},
+        {"max_salary": 143860, "min_salary": 54991,"date_posted": "2020-05-08"},
+        {"max_salary": 103279, "min_salary": 94715,"date_posted": "2020-05-05"},
+        {"max_salary": 148734, "min_salary": 122296,"date_posted": "2020-04-28"},
+        {"max_salary": 212901, "min_salary": 125410,"date_posted": "2020-04-28"},
+    ]
