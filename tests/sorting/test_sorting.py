4707e80fd6d4631f361b1835746ae7e9235c7e28
Robertson <robertson.nunes@gmail.com>
2021-12-03 18:26:07 -0300

    req10: feat test_sorting_by_criteria(full)

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -5,12 +5,12 @@ jobs = [
     {
         "min_salary": "1500",
         "max_salary": "3500",
-        "date_posted": "2021-12-03"
+        "date_posted": "2021-12-02"
     },
     {
         "min_salary": "1000",
         "max_salary": "3000",
-        "date_posted": "2021-12-02"
+        "date_posted": "2021-12-03"
     },
     {
         "min_salary": "500",
@@ -19,7 +19,7 @@ jobs = [
     },
 ]
 
-expected = [
+expected_min_salary = [
     {
         "min_salary": "500",
         "max_salary": "1500",
@@ -28,19 +28,61 @@ expected = [
     {
         "min_salary": "1000",
         "max_salary": "3000",
-        "date_posted": "2021-12-02"
+        "date_posted": "2021-12-03"
     },
     {
         "min_salary": "1500",
         "max_salary": "3500",
+        "date_posted": "2021-12-02"
+    },
+]
+
+expected_max_salary = [
+    {
+        "min_salary": "1500",
+        "max_salary": "3500",
+        "date_posted": "2021-12-02"
+    },
+    {
+        "min_salary": "1000",
+        "max_salary": "3000",
         "date_posted": "2021-12-03"
+    },
+    {
+        "min_salary": "500",
+        "max_salary": "1500",
+        "date_posted": "2021-12-01"
+    },
+]
+
+expected_date_posted = [
+    {
+        "min_salary": "1000",
+        "max_salary": "3000",
+        "date_posted": "2021-12-03"
+    },
+    {
+        "min_salary": "1500",
+        "max_salary": "3500",
+        "date_posted": "2021-12-02"
+    },
+    {
+        "min_salary": "500",
+        "max_salary": "1500",
+        "date_posted": "2021-12-01"
     },
 ]
 
 
 def test_sort_by_criteria():
     sort_by(jobs, "min_salary")
-    assert jobs == expected
+    assert jobs == expected_min_salary
+
+    sort_by(jobs, "max_salary")
+    assert jobs == expected_max_salary
+
+    sort_by(jobs, "date_posted")
+    assert jobs == expected_date_posted
 
 
 with pytest.raises(ValueError, match="invalid sorting criteria: """):
