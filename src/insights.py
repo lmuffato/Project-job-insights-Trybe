1711dcf322ebc5b768ab37ebd1e5864ee9c2815e
viniciusfrancas019 <viniciusfrancas019@gmail.com>
2021-12-03 23:20:38 -0300

    remove coments on functions

--- src/insights.py
+++ src/insights.py
@@ -2,41 +2,12 @@ import pandas as pd
 
 
 def get_unique_job_types(path):
-
-    """Checks all different job types and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
     df = pd.read_csv(path)
     to_return = df['job_type'].unique()
     return to_return
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
     df = pd.DataFrame(jobs)
     s = list(df.loc[df['job_type'] == job_type].T.to_dict().items())
     to_return = []
@@ -47,20 +18,6 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
     df = pd.read_csv(path)
     pre_def_data = df['industry'].dropna()
     to_return = pre_def_data.unique()
@@ -69,20 +26,6 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
     df = pd.DataFrame(jobs)
     s = list(df.loc[df['industry'] == industry].T.to_dict().items())
     to_return = []
@@ -93,20 +36,6 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
     df = pd.read_csv(path)
     regex = "(^\d+$|^\d+\.\d+$)"
     max_s = df['max_salary'].dropna().apply(str)
@@ -116,20 +45,6 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
     df = pd.read_csv(path)
     regex = "(^\d+$|^\d+\.\d+$)"
     min_s = df['min_salary'].dropna().apply(str)
@@ -139,28 +54,6 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
-
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
     if (type(salary) is not int
             or "min_salary" not in job
             or "max_salary" not in job
--- src/jobs.py
+++ src/jobs.py
@@ -4,18 +4,6 @@ import csv
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
     with open(path) as file:
         my_data = csv.DictReader(file, delimiter=',', quotechar='"')
         heads, *data = my_data
