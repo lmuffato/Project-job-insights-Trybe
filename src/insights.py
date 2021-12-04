e7d37da42e9dc92d7581e34e9220a01c63b8b1d2
rafael <rafittu@outlook.com>
2021-12-04 10:29:26 -0300

    feat req 7

--- src/insights.py
+++ src/insights.py
@@ -13,9 +13,9 @@ def get_unique_job_types(path):
 
 def filter_by_job_type(jobs, job_type):
     filtered_jobs = []
-    for job in jobs:
-        if job["job_type"] == job_type:
-            filtered_jobs.append(job)
+    for row in jobs:
+        if row["job_type"] == job_type:
+            filtered_jobs.append(row)
     return filtered_jobs
 
 
@@ -30,21 +30,11 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    filtered_industry = []
+    for row in jobs:
+        if row["industry"] == industry:
+            filtered_industry.append(row)
+    return filtered_industry
 
 
 def get_max_salary(path):
