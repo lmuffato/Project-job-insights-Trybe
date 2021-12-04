4e835eda9d61d971823f72d5c9c5004602da3827
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-12-04 00:30:44 -0300

    Req 8 - match salary range

--- src/insights.py
+++ src/insights.py
@@ -159,7 +159,14 @@ def matches_salary_range(job, salary):
         If `job["min_salary"]` is greather than `job["max_salary"]`
         If `salary` isn't a valid integer
     """
-    pass
+    if (type(salary) is not int
+       or "max_salary" not in job
+       or "min_salary" not in job
+       or type(job["min_salary"]) is not int
+       or type(job["max_salary"]) is not int
+       or job["min_salary"] > job["max_salary"]):
+        raise ValueError
+    return job["min_salary"] <= salary <= job["max_salary"]
 
 
 def filter_by_salary_range(jobs, salary):
