ead1f22032f88b5bde40f05f2d86dd1643c97099
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-12-03 22:38:11 -0300

    create fct filter_by_job_type(req 06)

--- src/insights.py
+++ src/insights.py
@@ -11,21 +11,11 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    list_of_jobs = []
+    for job in jobs:
+        if(job['job_type'] == job_type):
+            list_of_jobs.append(job)
+    return list_of_jobs
 
 
 def get_unique_industries(path):
