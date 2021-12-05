27ac3933cf444fc4d2e4550c91865246e573b0e8
w-cypriano <profwellington17@gmail.com>
2021-12-04 22:55:51 -0300

    req3-  implementação função get_unique_industries

--- src/insights.py
+++ src/insights.py
@@ -29,21 +29,12 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    jobs_list = read(path)
+    jobs_industry = set()
+    for row in jobs_list:
+        if(row['industry']) != "":
+            jobs_industry.add(row['industry'])
+    return jobs_industry
 
 
 def filter_by_industry(jobs, industry):
