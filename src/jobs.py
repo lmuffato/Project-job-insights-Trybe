ea6bbc53cdadd6876a39c38081b5e343587f765b
Nathalia Zebral <natzebral@gmail.com>
2021-12-05 15:27:05 -0300

    start first project with Python - req 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,13 @@
+import csv
 from functools import lru_cache
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path, "r") as file_jobs:
+        data = []
+        new_file = csv.DictReader(file_jobs)
+        for x in new_file:
+            data.append(x)
+            
+    return data
