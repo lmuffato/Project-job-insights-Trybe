4d6031272dc734cf7b34b06dac0bcd2b60798a45
PollyanaOliveira <pollyana.oliveir@gmail.com>
2021-12-05 13:56:41 -0400

    feat: :sparkles: try create req1

--- src/jobs.py
+++ src/jobs.py
@@ -1,19 +1,9 @@
 from functools import lru_cache
-# import csv
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path) as file:
+        reader = csv.DictReader(file, delimiter=",", quotechar='"')
+    return [row for row in reader]
