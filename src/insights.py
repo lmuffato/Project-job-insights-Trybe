4b12a04e37e1be6e5bc3bc88682a73aa1d2e0e2f
luanlsr <luanfswd@gmail.com>
2021-12-05 14:44:34 -0300

    feat: :sparkles: req 9 implemented

--- src/insights.py
+++ src/insights.py
@@ -72,18 +72,11 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    jobs_salary_range = []
+    for job in jobs:
+        try:
+            if (matches_salary_range(job, salary)):
+                jobs_salary_range.append(job)
+        except ValueError:
+            pass
+    return jobs_salary_range
