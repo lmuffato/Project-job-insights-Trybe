67f1e6794f965a5096813c5636b93b750d63a907
Mathaus Annibal <mathausannibal@gmail.com>
2021-12-05 16:09:03 -0300

    adiciona requisito 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,12 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    try:
+        with open(path, 'r') as file:
+            data = csv.DictReader(file)
+            return [line for line in data]
+    except OSError:
+        print('Arquivo nao localizado')
