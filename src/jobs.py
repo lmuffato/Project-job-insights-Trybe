f355043d30383859163907f6c5bbb623c6fe5662
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-12-04 23:27:36 -0300

    feat: requisito 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,6 +1,16 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    return []
+    with open(path, mode='r') as csv_file:
+        jobs = list(csv.DictReader(csv_file, delimiter=",", quotechar='"'))
+        # list compreension:
+        # list_jobs = [job for job in jobs]
+
+        # list
+        # list_jobs = []
+        # for job in jobs:
+        #     list_jobs.append(job)
+    return jobs
