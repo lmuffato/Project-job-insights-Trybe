32882b1f51b6587c26ce1a01849552d8ef7c2c38
Nathalia Zebral <natzebral@gmail.com>
2021-12-05 16:08:02 -0300

    req 7

--- src/insights.py
+++ src/insights.py
@@ -31,21 +31,12 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
+    filter_industry = []
+    for x in jobs:
+        if x["industry"] == industry:
+            filter_industry.append(x)
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    return filter_industry
 
 
 def get_max_salary(path):
