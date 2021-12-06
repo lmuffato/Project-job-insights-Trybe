b40466a56bd2595d763c50b9dff642d52086211e
Renan Costa <renancosta21@gmail.com>
2021-12-06 18:15:45 -0300

    req7

--- src/insights.py
+++ src/insights.py
@@ -40,21 +40,13 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
+    filtered_industries_list = []
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
+    for job in jobs:
+        if job["industry"] == industry:
+            filtered_industries_list.append(job)
 
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    return filtered_industries_list
 
 
 def get_max_salary(path):
