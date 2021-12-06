dde1bcc8e0caaa5f4341155dd17e3fffec40f089
Marcelo Maurício <mmauricio.design@gmail.com>
2021-12-06 10:24:15 -0300

    Feat 10: testing the cases of create test

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -2,10 +2,43 @@ import pytest
 
 from src.sorting import sort_by
 
+from src.jobs import read
+
+list_of_jobs_from_mocks = read('tests/mocks/jobs_with_salaries.csv')
+
+list_of_jobs_by_minimum = [
+    list_of_jobs_from_mocks[1],
+    list_of_jobs_from_mocks[3],
+    list_of_jobs_from_mocks[2],
+    list_of_jobs_from_mocks[0],
+]
+
+list_of_jobs_by_maximum = [
+    list_of_jobs_from_mocks[3],
+    list_of_jobs_from_mocks[2],
+    list_of_jobs_from_mocks[1],
+    list_of_jobs_from_mocks[0],
+]
+
+
+print(list_of_jobs_from_mocks)
+
 
 def test_sort_by_criteria():
-    'Testa se dá erro quando não passado os parâmetros'
-    with pytest.raises(TypeError):
-        sort_by()
+    # print(list_of_jobs_from_mocks, 'E o outro \n', list_of_jobs_to_compare)
+    'Testa se o valor não é diferente quando passado as informações certas'
+    sort_by(list_of_jobs_from_mocks, 'min_salary')
+    assert (list_of_jobs_from_mocks[0] == list_of_jobs_by_minimum[0])
 
-        pass
+    sort_by(list_of_jobs_from_mocks, 'max_salary')
+    assert (list_of_jobs_from_mocks[0] != list_of_jobs_by_maximum[3])
+
+    # 'Testa se o valor não é diferente quando passado as informações certas'
+    # jobs_sort_by_minimum = sort_by(list_of_jobs_from_mocks,
+    #                                'max_salary')
+    # assert jobs_sort_by_minimum[0] == list_of_jobs_from_mocks[1]
+
+
+'Testa se dá erro quando não passado os parâmetros'
+with pytest.raises(TypeError):
+    sort_by()
