19bf08a494928d3d583e096e462c439e0624741b
Patrick Morais <ppternunes@gmail.com>
2021-12-06 18:04:28 -0300

    req 5

--- src/insights.py
+++ src/insights.py
@@ -117,7 +117,12 @@ def get_min_salary(path):
     int
         The minimum salary paid out of all job opportunities
     """
-    pass
+    jobs = read(path)
+    salaries = set(
+        job["min_salary"] for job in jobs if job["min_salary"].isnumeric()
+    )
+    min_salary = min(salaries)
+    return min_salary
 
 
 def matches_salary_range(job, salary):
