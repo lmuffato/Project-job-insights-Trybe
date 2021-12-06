63f6763496aa0852beb95349f22eebef859b27f7
AndreBarroso <andre_barroso17@hotmail.com>
2021-12-06 09:48:10 -0300

    feat: test_sort_by_criteria[sort_by]

--- src/jobs.py
+++ src/jobs.py
@@ -4,19 +4,6 @@ import csv
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-
     with open(path, mode="r") as file:
         reader = csv.DictReader(file)
         data = [row for row in reader]
--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,59 @@
-# from src.sorting import sort_by
+# import pytest
+
+from src.sorting import sort_by
+
+# @pytest.fixture
+# def criteria_list():
+#     data = [
+#             {"min_salary": "3500", "max_salary": "6000", "date_posted": "2021-12-03"},
+#             {"min_salary": "3000", "max_salary": "7000", "date_posted": "2021-12-04"},
+#             {"min_salary": "8000", "max_salary": "11000", "date_posted": "2021-12-01"},
+#     ]
+
+
+criteria_list = [
+    {"min_salary": "3500", "max_salary": "6000", "date_posted": "2021-12-03"},
+    {"min_salary": "3000", "max_salary": "7000", "date_posted": "2021-12-04"},
+    {"min_salary": "8000", "max_salary": "11000", "date_posted": "2021-12-01"},
+]
+
+
+criteria_list_sorted_by_min_salary_output = [
+    {"min_salary": "3000", "max_salary": "7000", "date_posted": "2021-12-04"},
+    {"min_salary": "3500", "max_salary": "6000", "date_posted": "2021-12-03"},
+    {"min_salary": "8000", "max_salary": "11000", "date_posted": "2021-12-01"},
+]
+
+criteria_list_sorted_by_max_salary_output = [
+    {"min_salary": "8000", "max_salary": "11000", "date_posted": "2021-12-01"},
+    {"min_salary": "3000", "max_salary": "7000", "date_posted": "2021-12-04"},
+    {"min_salary": "3500", "max_salary": "6000", "date_posted": "2021-12-03"},
+]
+
+criteria_list_sorted_by_date_posted_output = [
+    {"min_salary": "3000", "max_salary": "7000", "date_posted": "2021-12-04"},
+    {"min_salary": "3500", "max_salary": "6000", "date_posted": "2021-12-03"},
+    {"min_salary": "8000", "max_salary": "11000", "date_posted": "2021-12-01"},
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(criteria_list, "min_salary")
+    assert criteria_list_sorted_by_min_salary_output
+
+    sort_by(criteria_list, "max_salary")
+    assert criteria_list_sorted_by_max_salary_output
+
+    sort_by(criteria_list, "date_posted")
+    assert criteria_list_sorted_by_date_posted_output
+
+    # with pytest.raises(
+    #     ValueError, match=f"invalid sorting criteria: {criteria_list}"
+    # ):
+    #     sort_by(criteria_list, criteria_list)
+
+    # with pytest.raises(ValueError, match="invalid sorting criteria: " ""):
+    #     sort_by(criteria_list, "")
+
+    # with pytest.raises(ValueError, match="invalid sorting criteria: None"):
+    #     sort_by(criteria_list, None)
