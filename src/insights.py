1cab59c36d4663478a4abacf6bfac0a514f28283
PollyanaOliveira <pollyana.oliveir@gmail.com>
2021-12-05 20:52:36 -0400

    feat: :sparkles: req9

--- src/insights.py
+++ src/insights.py
@@ -52,18 +52,12 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
+    jobs_filter_by_salary = []
 
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    for job in jobs:
+        try:
+            if matches_salary_range(job, salary):
+                jobs_filter_by_salary.append(job)
+        except ValueError:
+            pass
+    return jobs_filter_by_salary
