8e4afa329c2525c2d0c96b2fd4205efaba0704f9
Michael <michaelpetterson06.com.br>
2021-12-06 12:46:21 -0300

    req7

--- src/insights.py
+++ src/insights.py
@@ -23,21 +23,11 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    total = []
+    for line in jobs:
+        if line['industry'] == industry:
+            total.append(line)
+    return total
 
 
 def filter_list(row):
