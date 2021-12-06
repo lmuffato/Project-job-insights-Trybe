925cae6625e394aaf5cb423025b474233e802281
Michael <michaelpetterson06.com.br>
2021-12-06 15:53:19 -0300

    req9

--- src/insights.py
+++ src/insights.py
@@ -78,18 +78,13 @@ def matches_salary_range(job, salary):
 
 
 def filter_by_salary_range(jobs, salary):
-    """Filters a list of jobs by salary range
-
-    Parameters
-    ----------
-    jobs : list
-        The jobs to be filtered
-    salary : int
-        The salary to be used as filter
-
-    Returns
-    -------
-    list
-        Jobs whose salary range contains `salary`
-    """
-    return []
+    match_jobs = []
+    for job in jobs:
+        try:
+            result = matches_salary_range(job, salary)
+        except ValueError:
+            pass
+        else:
+            if result:
+                match_jobs.append(job)
+    return match_jobs
