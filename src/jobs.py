f476d563ffd4760724e7b46ba6e65c9dce60e992
Maria Baeta <m.martinsdg@gmail.com>
2021-12-06 20:49:14 -0300

    feat: :sparkles: Requisito 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,10 @@
+import csv
 from functools import lru_cache
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path, "r") as file:
+        arq_csv = csv.reader(file, delimiter=",")
+        data = list(arq_csv)
+    return [data]
