29573fafeb9a2c4477f3e2bb3843d9a5aa686f92
Renan Costa <renancosta21@gmail.com>
2021-12-06 18:04:55 -0300

    req6

--- src/insights.py
+++ src/insights.py
@@ -18,21 +18,13 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
+    filtered_job_types_list = []
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
+    for job in jobs:
+        if job["job_type"] == job_type:
+            filtered_job_types_list.append(job)
 
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    return filtered_job_types_list
 
 
 def get_unique_industries(path):
