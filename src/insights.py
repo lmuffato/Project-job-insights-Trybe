e290763454f0efb0c9175fa9d9c7854ce73a69cb
VRodriguera <V.rodriguesjk@gmail.com>
2021-12-06 19:09:26 -0300

    Feat: req 8

--- src/insights.py
+++ src/insights.py
@@ -60,8 +60,16 @@ def filter_by_industry(jobs, industry):
 
 
 def matches_salary_range(job, salary):
+    if ("min_salary" not in job or "max_salary" not in job):
+        raise ValueError("err")
+    if (type(job["max_salary"]) != int or type(job["min_salary"]) != int):
+        raise ValueError("err")
+    if (job["min_salary"] > job["max_salary"]):
+        raise ValueError("err")
+    if (type(salary) != int):
+        raise ValueError("err")
 
-    pass
+    return (job["min_salary"] <= salary <= job["max_salary"])
 
 
 def filter_by_salary_range(jobs, salary):
