c2a8860df5f8ce682beacce3ac116d32818192ed
AndreBarroso <andre_barroso17@hotmail.com>
2021-12-06 10:33:33 -0300

    fix: modifica valor denentrada para teste com chave errada

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -51,7 +51,7 @@ def test_sort_by_criteria():
     assert criteria_list_sorted_by_date_posted_output
 
     with pytest.raises(ValueError):
-        assert sort_by(criteria_list, "any element")
+        assert sort_by(criteria_list, "wrong_string")
 
     with pytest.raises(ValueError):
         sort_by(criteria_list, "")
