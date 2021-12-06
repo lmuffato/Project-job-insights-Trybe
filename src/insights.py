ddb79982261cad5c4e04d5f216f5d86008213ea1
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-12-06 19:12:10 -0400

    feat: req 5 - Implemente a função get_min_salary

--- src/insights.py
+++ src/insights.py
@@ -73,21 +73,20 @@ def get_max_salary(path):
 
 
 def get_min_salary(path):
-    """Get the minimum salary of all jobs
 
-    Must call `read`
+    list_data = read(path)
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
+    lower_salary = 99999999
 
-    Returns
-    -------
-    int
-        The minimum salary paid out of all job opportunities
-    """
-    pass
+    for job in list_data:
+        if (
+            job["min_salary"] != ""
+            and job["min_salary"] != "invalid"
+            and int(job["min_salary"]) < lower_salary
+        ):
+            lower_salary = int(job["min_salary"])
+
+    return lower_salary
 
 
 def matches_salary_range(job, salary):
