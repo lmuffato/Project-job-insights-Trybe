0050090277df0524d759018a976f0b5fc524d2fd
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-12-06 01:20:31 -0300

    Req 10: Test min_salary

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -20,8 +20,30 @@ jobs1 = [
      "date_posted": "2021/11/08"}
 ]
 
+jobs2 = [
+    {
+     "min_salary": 1000,
+     "max_salary": 5000,
+     "date_posted": "2021/11/08"},
+    {
+     "min_salary": 2000,
+     "max_salary": 4000,
+     "date_posted": "2021/11/10"},
+    {
+     "min_salary": 3000,
+     "max_salary": 99000,
+     "date_posted": "2021/11/11"},
+    {
+     "min_salary": 8000,
+     "max_salary": 15000,
+     "date_posted": "2021/11/09"},
+
+    ]
+
 
 def test_sort_by_criteria():
+    sort_by(jobs1, "min_salary")
+    assert jobs1 == jobs2
     with pytest.raises(ValueError):
         assert sort_by([], "oioioio") is None
     with pytest.raises(ValueError):
