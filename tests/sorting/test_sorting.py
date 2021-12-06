d455d4504160df91c85c18bd82c11ddb362bd2bd
nascjoao <oi@joaonasc.dev>
2021-12-06 16:45:21 -0300

    feat: tests for sort_by

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,46 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    mock_jobs = [
+        {
+            'max_salary': 5500,
+            'min_salary': 3500,
+            'date_posted': '2021-06-05'
+        },
+        {
+            'max_salary': 7500,
+            'min_salary': 5500,
+            'date_posted': '2021-06-06'
+        },
+        {
+            'max_salary': 10000,
+            'min_salary': 7500,
+            'date_posted': '2021-06-04'
+        },
+    ]
+
+    sorted_by_min_salary = [
+        mock_jobs[0],
+        mock_jobs[1],
+        mock_jobs[2],
+    ]
+
+    sorted_by_max_salary = [
+        mock_jobs[2],
+        mock_jobs[1],
+        mock_jobs[0],
+    ]
+
+    sorted_by_date_posted = [
+        mock_jobs[1],
+        mock_jobs[0],
+        mock_jobs[2],
+    ]
+
+    sort_by(mock_jobs, 'min_salary')
+    assert mock_jobs == sorted_by_min_salary
+    sort_by(mock_jobs, 'max_salary')
+    assert mock_jobs == sorted_by_max_salary
+    sort_by(mock_jobs, 'date_posted')
+    assert mock_jobs == sorted_by_date_posted
