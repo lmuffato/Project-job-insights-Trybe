e7659056245bf013b652ba3a95a8a0a4de7b410b
Patrick Morais <ppternunes@gmail.com>
2021-12-06 17:42:30 -0300

    req 3

--- src/insights.py
+++ src/insights.py
@@ -54,7 +54,11 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    jobs = read(path)
+    industries = set(
+        job["industry"] for job in jobs if job["industry"] is not None
+    )
+    return industries
 
 
 def filter_by_industry(jobs, industry):
