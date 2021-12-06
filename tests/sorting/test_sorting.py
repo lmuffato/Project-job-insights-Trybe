bf8c42fba25446437801757ce85e3a5625692671
rafael <rafittu@outlook.com>
2021-12-06 16:48:05 -0300

    feat req 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,32 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+
+jobs = [
+    {
+        "min_salary": 4000,
+        "max_salary": 5000,
+    },
+    {
+        "min_salary": 3000,
+        "max_salary": 4000,
+    },
+    {
+        "min_salary": 2000,
+        "max_salary": 3000,
+    },
+    {
+        "min_salary": 1000,
+        "max_salary": 2000,
+    },
+]
+
+sort_by_min_salary = [jobs[3], jobs[2], jobs[1], jobs[0]]
+sort_by_max_salary = [jobs[0], jobs[1], jobs[2], jobs[3]]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs, "min_salary")
+    assert jobs == sort_by_min_salary
+
+    sort_by(jobs, "max_salary")
+    assert jobs == sort_by_max_salary
