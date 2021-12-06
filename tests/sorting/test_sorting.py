dfafe156f433145219c3caa4253a07f0ce53f257
Marcelo Maurício <mmauricio.design@gmail.com>
2021-12-06 09:28:58 -0300

    Feat 10: created a test to receive a TypeError when called the function without parameters

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,11 @@
-# from src.sorting import sort_by
+import pytest
+
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    'Testa se dá erro quando não passado os parâmetros'
+    with pytest.raises(TypeError):
+        sort_by()
+
+        pass
