1030e3c69677f716c0a6da20232dbe3ec40594ff
Patrick Morais <ppternunes@gmail.com>
2021-12-06 18:07:28 -0300

    req 6

--- src/insights.py
+++ src/insights.py
@@ -36,7 +36,10 @@ def filter_by_job_type(jobs, job_type):
     list
         List of jobs with provided job_type
     """
-    return []
+    filtered_jobs = set(
+        job["job_type"] for job in jobs if job["job_type"] == job_type
+    )
+    return filtered_jobs
 
 
 def get_unique_industries(path):
