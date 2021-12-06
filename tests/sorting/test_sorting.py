528af7545917f025f008ab65d99d3cf1f9681b88
Eduardo Costa Maciel <costaeduardomaciel@gmail.com>
2021-12-06 10:30:29 -0300

    Fix: (req-10) modifica a ordem dos testes para a funçao sort_by

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -3,19 +3,19 @@ import pytest
 
 list_jobs = [
     {
-        "min_salary": 3500.00,
-        "max_salary": 5500.00,
-        "date_posted": "02/11/2019",
-    },
-    {
-        "min_salary": 1500.00,
-        "max_salary": 3500.00,
-        "date_posted": "02/11/2020",
-    },
-    {
-        "min_salary": 4000.00,
-        "max_salary": 9000.00,
+        "min_salary": 3000,
+        "max_salary": 5500,
         "date_posted": "02/11/2021",
+    },
+    {
+        "min_salary": 1500,
+        "max_salary": 3500,
+        "date_posted": "01/11/2021",
+    },
+    {
+        "min_salary": 4000,
+        "max_salary": 9000,
+        "date_posted": "03/11/2021",
     },
 ]
 
@@ -24,59 +24,61 @@ def test_sort_by_criteria():
     sort_by(list_jobs, "min_salary")
     assert list_jobs == [
         {
-            "min_salary": 3500.00,
-            "max_salary": 5500.00,
-            "date_posted": "02/11/2019",
+            "min_salary": 1500,
+            "max_salary": 3500,
+            "date_posted": "01/11/2021",
         },
         {
-            "min_salary": 1500.00,
-            "max_salary": 3500.00,
-            "date_posted": "02/11/2020",
-        },
-        {
-            "min_salary": 4000.00,
-            "max_salary": 9000.00,
+            "min_salary": 3000,
+            "max_salary": 5500,
             "date_posted": "02/11/2021",
+        },
+        {
+            "min_salary": 4000,
+            "max_salary": 9000,
+            "date_posted": "03/11/2021",
         },
     ]
 
     sort_by(list_jobs, "max_salary")
     assert list_jobs == [
         {
-            "min_salary": 4000.00,
-            "max_salary": 9000.00,
+            "min_salary": 4000,
+            "max_salary": 9000,
+            "date_posted": "03/11/2021",
+        },
+        {
+            "min_salary": 3000,
+            "max_salary": 5500,
             "date_posted": "02/11/2021",
         },
         {
-            "min_salary": 3500.00,
-            "max_salary": 5500.00,
-            "date_posted": "02/11/2019",
-        },
-        {
-            "min_salary": 1500.00,
-            "max_salary": 3500.00,
-            "date_posted": "02/11/2020",
+            "min_salary": 1500,
+            "max_salary": 3500,
+            "date_posted": "01/11/2021",
         },
     ]
 
     sort_by(list_jobs, "date_posted")
     assert list_jobs == [
         {
-            "min_salary": 1500.00,
-            "max_salary": 3500.00,
-            "date_posted": "02/11/2020",
+            "min_salary": 4000,
+            "max_salary": 9000,
+            "date_posted": "03/11/2021",
         },
         {
-            "min_salary": 4000.00,
-            "max_salary": 9000.00,
+            "min_salary": 3000,
+            "max_salary": 5500,
             "date_posted": "02/11/2021",
         },
         {
-            "min_salary": 3500.00,
-            "max_salary": 5500.00,
-            "date_posted": "02/11/2019",
+            "min_salary": 1500,
+            "max_salary": 3500,
+            "date_posted": "01/11/2021",
         },
     ]
 
-    with pytest.raises(ValueError, match="invalid sorting criteria: invalid"):
-        sort_by(list_jobs, "invalid")
+    with pytest.raises(
+        ValueError, match="invalid sorting criteria: invalid_criteria"
+    ):
+        sort_by(list_jobs, "invalid_criteria")
