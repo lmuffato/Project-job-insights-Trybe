38c711270c4dab65d722267bcf74b6b8bdbd17a6
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-12-06 01:09:44 -0300

    Req 10 - Fix something

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,7 +1,28 @@
 import pytest
 from src.sorting import sort_by
 
+jobs1 = [
+    {
+     "min_salary": 3000,
+     "max_salary": 99000,
+     "date_posted": "2021/11/11"},
+    {
+     "min_salary": 2000,
+     "max_salary": 4000,
+     "date_posted": "2021/11/10"},
+    {
+     "min_salary": 8000,
+     "max_salary": 15000,
+     "date_posted": "2021/11/09"},
+    {
+     "min_salary": 1000,
+     "max_salary": 5000,
+     "date_posted": "2021/11/08"}
+]
+
 
 def test_sort_by_criteria():
     with pytest.raises(ValueError):
-        sort_by("", "")
+        assert sort_by([], "oioioio") is None
+    with pytest.raises(ValueError):
+        assert sort_by(jobs1, "") is None
