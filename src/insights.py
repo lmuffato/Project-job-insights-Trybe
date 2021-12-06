105c3cdfb00de3d129e38f5fb1be67f7ed171813
Fernando Castello Branco <fernandocastellob@gmail.com>
2021-12-06 14:41:46 -0300

    FEAT:get_unique_industries

--- src/insights.py
+++ src/insights.py
@@ -27,21 +27,9 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    jobs = read(path)
+    unique_industries = set(job["industry"] for job in jobs if job["industry"])
+    return unique_industries
 
 
 def filter_by_industry(jobs, industry):
