b3bf59f81f07dc8933c0172cc26a6c7b6abd6fd2
Gabriel Ferreira <glucas709@gmail.com>
2021-12-06 12:12:20 -0300

    feat: requiriment 6

--- src/insights.py
+++ src/insights.py
@@ -7,21 +7,8 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    filtered = filter(lambda job: job['job_type'] == job_type, jobs)
+    return list(filtered)
 
 
 def get_unique_industries(path):
