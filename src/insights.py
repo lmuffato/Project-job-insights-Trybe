a41802d116bd9d4f5090397a60c3ea9166f1cb64
Michael <michaelpetterson06.com.br>
2021-12-05 23:29:11 -0300

    req3

--- src/insights.py
+++ src/insights.py
@@ -26,21 +26,10 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    with open(path, mode='r') as file:
+        file_reader = csv.DictReader(file, delimiter=',')
+        row = [row["industry"] for row in file_reader]
+        return [industry for industry in filter(None, (list(set(row))))]
 
 
 def filter_by_industry(jobs, industry):
