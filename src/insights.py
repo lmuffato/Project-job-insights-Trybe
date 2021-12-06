aaaedae61d6ce1faebd63c2824885c7214cdd33c
Gabriel Ferreira <glucas709@gmail.com>
2021-12-05 21:55:34 -0300

    feat: requiriment 3

--- src/insights.py
+++ src/insights.py
@@ -25,21 +25,15 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    jobs = read(path)
 
-    Must call `read`
+    industries = list(
+        set([unique_industry["industry"]
+             for unique_industry in jobs
+             if unique_industry["industry"] != ""])
+    )
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return industries
 
 
 def filter_by_industry(jobs, industry):
