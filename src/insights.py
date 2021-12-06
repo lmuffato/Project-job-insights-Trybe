fe1acdcd4d056d87df70f0c519d3cdb038592215
Gabriel Ferreira <glucas709@gmail.com>
2021-12-06 12:15:47 -0300

    feat: requiriment 7

--- src/insights.py
+++ src/insights.py
@@ -24,21 +24,8 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    filtered = filter(lambda job: job['industry'] == industry, jobs)
+    return list(filtered)
 
 
 def get_max_salary(path):
