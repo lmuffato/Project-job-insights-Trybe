307806f77dc4b1b1e466c1992882f421de56738b
Maria Baeta <m.martinsdg@gmail.com>
2021-12-06 21:02:39 -0300

    refactor: :recycle: Requisito 1

--- src/jobs.py
+++ src/jobs.py
@@ -1,10 +1,12 @@
 import csv
 from functools import lru_cache
 
+#  https://docs.python.org/pt-br/3/library/csv.html
+
 
 @lru_cache
 def read(path):
     with open(path, "r") as file:
-        arq_csv = csv.reader(file, delimiter=",")
+        arq_csv = csv.DictReader(file)
         data = list(arq_csv)
-    return [data]
+    return data
