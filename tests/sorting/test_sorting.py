3085459d195019119397f36823025743b7677464
Alexandre Oliveira <alexandremolive@gmail.com>
2021-12-07 18:45:50 -0300

    req 10 partial

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,20 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+import pytest
+
+jobs = [
+    {"min_salary": 0, "max_salary": 10, "date_posted": "01/01/2021"},
+    {"min_salary": 5, "max_salary": 15, "date_posted": "01/03/2021"},
+]
+
+sort_by_max_salary_mock = [
+    {"min_salary": 5, "max_salary": 15, "date_posted": "01/03/2021"},
+    {"min_salary": 0, "max_salary": 10, "date_posted": "01/01/2021"},
+]
 
 
 def test_sort_by_criteria():
-    pass
+    with pytest.raises(ValueError, match="invalid sorting criteria: nohing"):
+        sort_by(jobs, "nohing")
+
+    sort_by(jobs, "max_salary")
+    assert jobs == sort_by_max_salary_mock
