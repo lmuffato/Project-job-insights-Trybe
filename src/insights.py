1514a0506983a13faafa609fbdba65d0704d6eca
Elisa <elisafranca.carvalho@gmail.com>
2021-12-07 07:07:16 -0300

    [feat] Implements 7th requirement

--- src/insights.py
+++ src/insights.py
@@ -31,21 +31,12 @@ def get_unique_industries(path):
 
 
 def filter_by_industry(jobs, industry):
-    """Filters a list of jobs by industry
+    filtered_jobs = []
+    for job in jobs:
+        if job["industry"] == industry:
+            filtered_jobs.append(job)
 
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    return []
+    return filtered_jobs
 
 
 def get_max_salary(path):
