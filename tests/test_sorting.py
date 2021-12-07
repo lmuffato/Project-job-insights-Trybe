419eb8d45dc47fe7f87f7fd82e841787c9813e68
lmuffato <lmiremuffato@gmail.com>
2021-12-07 15:53:49 -0300

    requisito 9

--- tests/test_sorting.py
+++ tests/test_sorting.py
@@ -25,51 +25,51 @@ mock_input = [
 
 
 result_sorted_by_max_salary = [
-    # mock_input[0], mock_input[1], mock_input[2], mock_input[3]
-    {
-        "max_salary": 120000,
-        "min_salary": 80001,
-        "date_posted": "2021-04-01",
-    },
-    {
-        "max_salary": 80000,
-        "min_salary": 30001,
-        "date_posted": "2021-03-01",
-    },
-    {
-        "max_salary": 30000,
-        "min_salary": 10001,
-        "date_posted": "2021-02-01",
-    },
-    {
-        "max_salary": 10000,
-        "min_salary": 3000,
-        "date_posted": "2021-01-01",
-    },
+    mock_input[0], mock_input[1], mock_input[2], mock_input[3]
+    # {
+    #     "max_salary": 120000,
+    #     "min_salary": 80001,
+    #     "date_posted": "2021-04-01",
+    # },
+    # {
+    #     "max_salary": 80000,
+    #     "min_salary": 30001,
+    #     "date_posted": "2021-03-01",
+    # },
+    # {
+    #     "max_salary": 30000,
+    #     "min_salary": 10001,
+    #     "date_posted": "2021-02-01",
+    # },
+    # {
+    #     "max_salary": 10000,
+    #     "min_salary": 3000,
+    #     "date_posted": "2021-01-01",
+    # },
 ]
 
 result_sorted_by_min_salary = [
-    # mock_input[3], mock_input[2], mock_input[1], mock_input[0]
-    {
-        "max_salary": 10000,
-        "min_salary": 3000,
-        "date_posted": "2021-01-01",
-    },
-    {
-        "max_salary": 30000,
-        "min_salary": 10001,
-        "date_posted": "2021-02-01",
-    },
-    {
-        "max_salary": 80000,
-        "min_salary": 30001,
-        "date_posted": "2021-03-01",
-    },
-    {
-        "max_salary": 120000,
-        "min_salary": 80001,
-        "date_posted": "2021-04-01",
-    },
+    mock_input[3], mock_input[2], mock_input[1], mock_input[0]
+    # {
+    #     "max_salary": 10000,
+    #     "min_salary": 3000,
+    #     "date_posted": "2021-01-01",
+    # },
+    # {
+    #     "max_salary": 30000,
+    #     "min_salary": 10001,
+    #     "date_posted": "2021-02-01",
+    # },
+    # {
+    #     "max_salary": 80000,
+    #     "min_salary": 30001,
+    #     "date_posted": "2021-03-01",
+    # },
+    # {
+    #     "max_salary": 120000,
+    #     "min_salary": 80001,
+    #     "date_posted": "2021-04-01",
+    # },
 ]
 
 
