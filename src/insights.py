1983b60d2443ee23b305ef96fbb3a0578f202234
Elisa <elisafranca.carvalho@gmail.com>
2021-12-06 21:46:29 -0300

    [feat] Implements 3rd requirement

--- src/insights.py
+++ src/insights.py
@@ -30,21 +30,13 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
+    list = read(path)
+    industries_quantity = set()
+    for row in list:
+        if row["industry"] != "":
+            industries_quantity.add(row["industry"])
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    return industries_quantity
 
 
 def filter_by_industry(jobs, industry):
