3068caf29c811c07fc34fdae7c70d1ba45bbc4b9
Hernanne Domiciano <78621976+hernannegp@users.noreply.github.com>
2021-12-07 11:50:18 -0300

    feat: testes da função sort by

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,40 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+
+job_list = [
+    {
+        "name": "junior developer",
+        "min_salary": 2500,
+        "max_salary": 4500
+    },
+    {
+        "name": "senior developer",
+        "min_salary": 9000,
+        "max_salary": 15000,
+    },
+    {
+        "name": "pleno developer",
+        "min_salary": 5000,
+        "max_salary": 8000,
+    }
+]
+
+sort_by_min_salary = [
+    job_list[0],
+    job_list[2],
+    job_list[1]
+]
+
+sort_by_max_salary = [
+    job_list[1],
+    job_list[2],
+    job_list[0]
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(job_list, "min_salary")
+    assert(job_list == sort_by_min_salary)
+
+    sort_by(job_list, "max_salary")
+    assert(job_list == sort_by_max_salary)
