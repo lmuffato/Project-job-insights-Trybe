59edec39e89743ad297f8fd1b7a4cbc1f350cd6d
Edrei <edreimateus@gmail.com>
2021-12-07 21:21:22 -0300

    feat 1 requiriment

--- src/jobs.py
+++ src/jobs.py
@@ -1,8 +1,12 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
+    with open(path) as file:
+        return list(csv.DictReader(file))
+
     """Reads a file from a given path and returns its contents
 
     Parameters
@@ -15,4 +19,4 @@ def read(path):
     list
         List of rows as dicts
     """
-    return []
+# ref: https://stackoverflow.com/questions/53917691/creating-a-list-of-objects-with-a-class-in-python
