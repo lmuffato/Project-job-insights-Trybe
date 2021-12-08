16668cc9e18f075eb725b1afefac70fdee698fa8
Gustavo Sousa <gustavohl98@gmail.com>
2021-12-07 22:46:17 -0300

    Req 1 finished - implements read csv function

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,12 @@
 from functools import lru_cache
+import csv
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    with open(path) as my_file:
+        my_file_dicts = csv.DictReader(my_file)
+        my_list = []
+        for each_dict in my_file_dicts:
+            my_list.append(each_dict)
+        return my_list
