71cd599ce29e3aff5de3ac2c84e76db37e857386
Edrei <edreimateus@gmail.com>
2021-12-08 00:09:57 -0300

    feat 3 requiriment

--- src/insights.py
+++ src/insights.py
@@ -43,6 +43,13 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
+    jobs = read(path)
+    industries = set(
+        job['industry'] 
+        for job in jobs
+        if job['industry']
+    )
+    return industries
     """Checks all different industries and returns a list of them
 
     Must call `read`
@@ -57,7 +64,6 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
 
 
 def filter_by_industry(jobs, industry):
